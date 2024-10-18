efe2f243ac4ce38f051f41e5b297049a7780fc80
KevinLyons247 <kevinlyons247@gmail.com>
2024-10-18 10:44:04 -0400

    Initial commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,18 @@
+import './Task.css';
+
+export default function Task({ task, onChange }) {
+  return (
+    <div className={`task-card ${task.completed ? 'task-card-complete' : ''}`}>
+      <p
+        className={`task-title ${task.completed ? 'task-title-complete' : ''}`}
+      >
+        {task.value}
+      </p>
+      <input
+        type="checkbox"
+        onChange={(e) => onChange(e.target.value)}
+        className="task-check"
+      />
+    </div>
+  );
+}
